0568f9c740c4e48158157dcad1d0d30d64c9e3c9
songsong <1072047605@qq.com>
2026-06-19 17:51:24 +0800

    debug: remove template fade-in to test hydration

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-import { PageFadeIn } from "@/components/page-fade-in";
-
 export default function Template({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isHome = pathname === "/";
-
-  if (isHome) {
-    return children;
-  }
-
-  return <PageFadeIn className="flex min-h-full flex-1 flex-col">{children}</PageFadeIn>;
+  return children;
 }
